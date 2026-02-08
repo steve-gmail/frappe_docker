8c359378c1e70b1e56b8c6da5ab6cf06eb498418
steve-gmail <steveyi168@gmail.com>
2026-02-09 02:24:29 +0800

    Add after_build.sh for Docker site setup and app installation
    This script sets up Docker containers for the application, creates a new site with specified configurations, and installs multiple applications.

---
+++ after_build.sh
@@ -0,0 +1,18 @@
+sudo docker compose --env-file custom.env \
+  -f compose.yaml \
+  -f overrides/compose.mariadb.yaml \
+  -f overrides/compose.redis.yaml \
+  -f overrides/compose.noproxy.yaml \
+  config > compose.custom.yaml
+
+sudo docker compose -p frappe -f compose.custom.yaml up -d
+
+sudo docker compose -p frappe exec backend bench new-site \
+  --mariadb-user-host-login-scope='%' \
+  --admin-password=admin \
+  --db-root-username=root \
+  --db-root-password=root123 \
+  --install-app erpnext \
+  --set-default frontend
+
+sudo docker compose -p frappe exec backend bench --site frontend install-app hrms payments lending education crm print_designer erpnext_china
