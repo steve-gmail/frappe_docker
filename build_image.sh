0df8f0423586e65a621fe58df90be9c2d6dc0776
steve-gmail <steveyi168@gmail.com>
2026-02-09 19:06:34 +0800

    Change Frappe repository URL in build_image.sh
    Updated Frappe repository URL in build script.

--- build_image.sh
+++ build_image.sh
@@ -2,7 +2,7 @@ export APPS_JSON_BASE64=$(base64 -w 0 apps.json)
 echo -n ${APPS_JSON_BASE64} | base64 -d
 
 sudo docker build \
---build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
+--build-arg=FRAPPE_PATH=https://github.com/steve-gmail/frappe \
 --build-arg=FRAPPE_BRANCH=version-15 \
 --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
 --tag=myerp:version-15 \
